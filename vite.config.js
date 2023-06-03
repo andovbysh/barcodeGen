a962765a3d156a23713e14e3b9ecc1cbeacae772
theparranoid <andfer220@gmail.com>
2023-06-03 18:12:30 +0600

    Revert "fix proxy in vite.config.js"
    This reverts commit 33c79707fe822f884328ade4309edb2aa93df35e.

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         target: "https://products.aspose.app",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/asposetest\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
